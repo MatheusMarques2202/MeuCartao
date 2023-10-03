0cfce9a009d54be89567c4a7bcf2c4d2d87ad097
MatheusMarques2202 <matheuslima2203@gmail.com>
2023-10-03 11:08:09 -0300

    cartaooo

--- app/src/main/java/com/example/meucarto/MainActivity.kt
+++ app/src/main/java/com/example/meucarto/MainActivity.kt
@@ -3,18 +3,30 @@ package com.example.meucarto
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.meucarto.ui.theme.MeuCartãoTheme
 import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
@@ -24,7 +36,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MeuCartãoTheme {
-
+                textoCartao()
             }
         }
     }
@@ -32,26 +44,57 @@ class MainActivity : ComponentActivity() {
 @Preview(showSystemUi = true)
 @Composable
 fun textoCartao() {
-    Column (
-horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ){
 
-        Text(
-            text =
-                    "Feliz dias do programador ",
-            fontSize = 70.sp,
-            textAlign = TextAlign.Center
+    Box {
 
-        )
-        Text(
-            text = "para: EU ",
-            fontSize = 30.sp
+
+        Image(
+            painter = painterResource(id = R.drawable.dia_do_programador),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
 
-    }
-    Row {
 
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            Text(
+                text =
+                "Feliz dia do programador ",
+                fontSize = 70.sp,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Black,
+                fontFamily = FontFamily.Cursive,
+                color = Color.White,
+                lineHeight = 116.sp,
+                modifier = Modifier.padding(top = 150.dp)
+
+            )
+            Dedicatorio(nomes = "de prof")
+            Dedicatorio(nomes = "para matheus")
+            
+
+
+        }
     }
+}
+
+@Composable
+fun Dedicatorio(nomes:String){
+    Text(
+        text = nomes,
+        fontSize = 30.sp,
+        fontFamily = FontFamily.Cursive,
+        color = Color.Black,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 20.dp, bottom = 30.dp),
+        textAlign = TextAlign.Start,
+
+        )
+    
 }
